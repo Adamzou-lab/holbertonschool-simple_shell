62348a7c1efee12fe925b697f0d59d6bd9983050
nohamoulma-hub <noham.oulma@gmail.com>
2026-04-16 14:16:04 +0200

    Feat: add shell.h

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h> /** getline */
+#include <unistd.h> /** write, isaty, STDIN_FILENO, execve */
+#include <stdlib.h> /** free, exit */
+#include <sys/types.h> /** pid_t */
+#include <sys/wait.h> /** wait */
+#include <string.h> /** strcmp, strtok */
+#include <stddef.h>
+
+#endif
